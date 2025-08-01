e649ca55c677ad3100f7b0fa7cbf17cfb8d62713
maciej.nowak <maciej.nowak@tomtom.com>
2025-08-01 14:35:55 +0200

    fixes

--- src/main/java/com/task/store/KeyValueHistoryDTO.java
+++ src/main/java/com/task/store/KeyValueHistoryDTO.java
@@ -4,6 +4,6 @@ import jakarta.validation.constraints.NotNull;
 
 public record KeyValueHistoryDTO(Long id, @NotNull String key, @NotNull String value, @NotNull Long timestamp) {
     public KeyValueHistoryDTO(KeyValueHistory keyValueHistory){
-        this(keyValueHistory.getId(), keyValueHistory.getKey(), keyValueHistory.getValue(), keyValueHistory.getTimestamp()); // delegate to canonical constructor
+        this(keyValueHistory.getId(), keyValueHistory.getKey(), keyValueHistory.getValue(), keyValueHistory.getTimestamp());
     }
 }
